4a07094cbfabe8237eb70d5f4675ae1aab3345cc
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-06-30 08:37:01 +0500

    Add initial array

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,14 @@
 ﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк,
 длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клаиватуры,
 либо задать на старте выполнения алгоритма. */
-
 string[] array = new string[] { "Hello", "friends", "I", "glad", "to", "see", "you" };
+Console.Write($"Первоначальный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+Console.Write($"Массив, содержащий слова, состоящие из 3х и менее символов : ");
 for (int i = 0; i < array.Length; i++)
 {
     int str = array[i].Length;
